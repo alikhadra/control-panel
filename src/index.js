161692bb1392d0a91806bb0e5fe6484d1a9b1905
hsoubecommerce <alikhadra15@gmail.com>
2021-08-29 12:40:50 +0300

    finish components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
 import './assets/sass/style.scss';
 import 'normalize.css/normalize.css';
-
-
-(function () {
-    const closeBanners = document.querySelectorAll(".c-banner__close");
-    closeBanners.forEach(closeBanner => {
-        closeBanner.addEventListener("click", event => {
-            const banner = event.target.parentNode;
-            banner.classList.add("collapse");
-
-            banner.addEventListener("transitionend", function (event) {
-                if (event.target === this) {
-                    this.remove();
-                }
-            })
-        })
-    })
-})();
